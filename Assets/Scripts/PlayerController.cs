0b18cd8bfc6bc4fbe7cd79fc41bfd74f88962e6c
Jocelyn Beedie <jellonator00@gmail.com>
2020-09-28 00:58:47 -0400

    Apply input data to player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,15 +5,21 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    private Rigidbody rigidbody;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody = GetComponent<Rigidbody>();
     }
 
     void OnMove(InputValue movementValue)
     {
-        
+        Vector2 movementVector = movementValue.Get<Vector2>();
+    }
+
+    void FixedUpdate()
+    {
+
     }
 }
