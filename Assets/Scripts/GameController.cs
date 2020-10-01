eb6c07f00b1d8ba3ede4fb77de710ec396bee932
Jocelyn Beedie <jellonator00@gmail.com>
2020-10-01 15:41:49 -0400

    Fixed incorrect compass rotation

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -15,6 +15,8 @@ public class GameController : MonoBehaviour
     private Vector3 baseGravity = new Vector3(0f, -1f, 0f) * 25.0f;
     // Movement pulled from OnMove
     private Vector2 movement = Vector2.zero;
+    // Look value pulled from OnLook
+    private Vector2 look = Vector2.zero;
 
     // Handle inputs
     void OnMove(InputValue movementValue)
@@ -22,19 +24,25 @@ public class GameController : MonoBehaviour
         movement = movementValue.Get<Vector2>();
     }
 
+    void OnLook(InputValue lookValue)
+    {
+        look = lookValue.Get<Vector2>();
+    }
+
     // Update is called once per frame
     void Update()
     {
         // Get the view axis to rotate the game by
         Vector3 x_axis = cameraReference.transform.right;
-        Vector3 y_axis = cameraReference.transform.forward;
+        Vector3 y_axis = cameraReference.transform.up;
+        Vector3 z_axis = cameraReference.transform.forward;
         // Rotate the game around the axis
-        // Rotate around the X axis by the Y input, and vice versa
+        transform.RotateAround(Vector3.zero, y_axis, look.x);
         transform.RotateAround(Vector3.zero, x_axis, -movement.y);
-        transform.RotateAround(Vector3.zero, y_axis, movement.x);
+        transform.RotateAround(Vector3.zero, z_axis, movement.x);
         // Apply the rotation
         cameraParentReference.transform.rotation = transform.rotation;
         Physics.gravity = transform.rotation * baseGravity;
-        compassReference.transform.rotation = transform.rotation;
+        compassReference.transform.rotation = Quaternion.Inverse(transform.rotation);
     }
 }
